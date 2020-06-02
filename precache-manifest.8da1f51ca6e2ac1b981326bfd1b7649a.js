04f68304ff37920a170108c514619b75e5bd3cac
PortosaurusBot <claperone.portasaurus@gmail.com>
2020-06-02 19:31:28 +0100

    Auto generated portfolio

---
+++ precache-manifest.8da1f51ca6e2ac1b981326bfd1b7649a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34271944e15d8a0694e686a623b57723",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1877de172ec4a2b57860",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bb64744ce24d0b410f1",
+    "url": "/static/js/2.18e4ac39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18e4ac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1877de172ec4a2b57860",
+    "url": "/static/js/main.c1e6e39d.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
